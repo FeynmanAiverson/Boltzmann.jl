2182304238a0241b9155a9520cc3be523d0e1872
Eric W. Tramel <eric.tramel@gmail.com>
2015-09-22 15:27:15 +0200

    Adding validation to weight decay tests
    Included a validation set to the tests of l1 and l2 weight decay in
    order to see the potential advantage of these approaches in terms of
    generalisation performance.

--- test/testweightdecay.jl
+++ test/testweightdecay.jl
@@ -8,28 +8,31 @@ using Base.Test
 function run_mnist()
     X, y = testdata()  # test data is smaller, no need to downsample
     X = X ./ (maximum(X) - minimum(X))
+
+    TrainSet = X[:,1:7000]
+    ValidSet = X[:,7001:end]
     HiddenUnits = 500;
     Epochs = 10;
 
-    m = BernoulliRBM(28*28, HiddenUnits; momentum=0.5, dataset=X)
-    mwdQuad = BernoulliRBM(28*28, HiddenUnits; momentum=0.5 ,dataset=X)
-    mwdLin = BernoulliRBM(28*28, HiddenUnits; momentum=0.5, dataset=X)
+    m = BernoulliRBM(28*28, HiddenUnits; momentum=0.5, dataset=TrainSet)
+    mwdQuad = BernoulliRBM(28*28, HiddenUnits; momentum=0.5 ,dataset=TrainSet)
+    mwdLin = BernoulliRBM(28*28, HiddenUnits; momentum=0.5, dataset=TrainSet)
     
     # Attempt without weight decay
     info("Running Without Weight Decay")
-    fit(m, X;n_iter=Epochs,lr=0.005)
+    fit(m, TrainSet;n_iter=Epochs,lr=0.005,validation=ValidSet)
     chart_weights(m.W, (28, 28); annotation="No Weight Decay")
     chart_weights_distribution(m.W;filename="nodecay_distribution.pdf",bincount=200)
 
     # Attempt with L2 weight decay
     info("Running With L2-Decay")
-    fit(mwdQuad, X;n_iter=Epochs,weight_decay="l2",decay_magnitude=0.05,lr=0.005)
+    fit(mwdQuad, TrainSet;n_iter=Epochs,weight_decay="l2",decay_magnitude=0.05,lr=0.005,validation=ValidSet)
     chart_weights(mwdQuad.W, (28, 28);annotation="L2 Weight Decay")
     chart_weights_distribution(mwdQuad.W;filename="l2decay_distribution.pdf",bincount=200)
 
     # Attempt with L1 weight decay
     info("Running With L1-Decay")
-    fit(mwdLin, X;n_iter=Epochs,weight_decay="l1",decay_magnitude=0.05,lr=0.005)
+    fit(mwdLin, TrainSet;n_iter=Epochs,weight_decay="l1",decay_magnitude=0.05,lr=0.005,validation=ValidSet)
     chart_weights(mwdLin.W, (28, 28);annotation="L1 Weight Decay")
     chart_weights_distribution(mwdLin.W;filename="l1decay_distribution.pdf",bincount=200)
 
