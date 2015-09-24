67a59038a55de1c93e9af30a75c491b7cd2e27e9
marylou-gabrie <marylou.gabrie@ens.fr>
2015-09-24 17:59:57 +0200

    test file update

--- test/testTAP.jl
+++ test/testTAP.jl
@@ -12,37 +12,41 @@ function run_mnist()
     LearningRate = 0.1;
     
 
-    m = BernoulliRBM(28*28, HiddenUnits; momentum=0.0, dataset=X)
+    m1 = BernoulliRBM(28*28, HiddenUnits; momentum=0.5, dataset=X)
+    m2 = BernoulliRBM(28*28, HiddenUnits; momentum=0.5, dataset=X)
+    m3 = BernoulliRBM(28*28, HiddenUnits; momentum=0.5, dataset=X)
+    m4 = BernoulliRBM(28*28, HiddenUnits; momentum=0.5, dataset=X)
+
     mM = BernoulliRBM(28*28, HiddenUnits; momentum=0.5, dataset=X)
     
-    # info("Running Without Momentum With naive")
+    info("Running Without Momentum With naive")
+    tic()
+    fit(m1, X;n_iter=Epochs,lr=LearningRate, approx="naive",persistent=true, n_gibbs=3, weight_decay="l2",decay_magnitude=0.05)
+    toc()
+    chart_weights(m1.W, (28, 28); annotation="naive")
+    chart_weights_distribution(m1.W;filename="naive_distribution.pdf",bincount=200)
+
+    # info("Running Without Momentum With tap2")
     # tic()
-    # fit(mM, X;n_iter=Epochs,lr=LearningRate, approx="naive",persistent=false)
+    # fit(m2, X;n_iter=Epochs,lr=LearningRate, approx="tap2", persistent=true, n_gibbs=3, weight_decay="l2",decay_magnitude=0.05)
     # toc()
-    # chart_weights(mM.W, (28, 28); annotation="naive")
-    # chart_weights_distribution(mM.W;filename="naive_distribution.pdf",bincount=200)
+    # chart_weights(m2.W, (28, 28); annotation="tap2")
+    # chart_weights_distribution(m2.W;filename="tap2_distribution.pdf",bincount=200)
 
-    # info("Running Without Momentum With naive")
-    tic()
-    fit(mM, X;n_iter=Epochs,lr=LearningRate, approx="tap2", persistent=false)
-    toc()
-    chart_weights(mM.W, (28, 28); annotation="tap2")
-    chart_weights_distribution(mM.W;filename="tap2_distribution.pdf",bincount=200)
-
-    # info("Running Without Momentum With naive")
-    tic()
-    fit(mM, X;n_iter=Epochs,lr=LearningRate, approx="tap3",persistent=true)
-    toc()
-    chart_weights(mM.W, (28, 28); annotation="tap3")
-    chart_weights_distribution(mM.W;filename="tap3_distribution.pdf",bincount=200)
+    # info("Running Without Momentum With tap3")
+    # tic()
+    # fit(m3, X;n_iter=Epochs,lr=LearningRate, approx="tap3",persistent=true, n_gibbs=3, weight_decay="l2",decay_magnitude=0.05)
+    # toc()
+    # chart_weights(m3.W, (28, 28); annotation="tap3")
+    # chart_weights_distribution(m3.W;filename="tap3_distribution.pdf",bincount=200)
 
 
     # info("Running Without Momentum With CD")
     # tic()
-    # fit(m, X;n_iter=Epochs,lr=LearningRate, approx="CD",persistent=false) #, weight_decay="none",decay_magnitude=0.0)
+    # fit(m4, X;n_iter=Epochs,lr=LearningRate, approx="CD",persistent=true, n_gibbs=1, weight_decay="l2",decay_magnitude=0.05) #, weight_decay="l2",decay_magnitude=0.05)
     # toc()
-    # chart_weights(m.W, (28, 28); annotation="CD")
-    # chart_weights_distribution(m.W;filename="CD_distribution.pdf",bincount=200)
+    # chart_weights(m4.W, (28, 28); annotation="CD")
+    # chart_weights_distribution(m4.W;filename="CD_distribution.pdf",bincount=200)
 
 
     # info("Running Without Momentum With naive")
@@ -50,7 +54,7 @@ function run_mnist()
     # chart_weights(mM.W, (28, 28); annotation="naive non persistent")
     # chart_weights_distribution(mM.W;filename="naive_nonpersistent_distribution.pdf",bincount=200)
 
-    return m
+    #return m
 end
 
 run_mnist()
