df98f0a813ea5278cc084d986462f6000b0359f7
Eric W. Tramel <eric.tramel@gmail.com>
2015-11-18 13:47:56 +0100

    De-crufting Training.jl
    Removed some of the commented out portions, added a few new comments
    for clarity, introduced the new `random_columns` function. Updated
    things to match with the new monitor chart.

--- src/Training.jl
+++ src/Training.jl
@@ -12,34 +12,22 @@ using PyCall
 import StatsBase.fit
 
 function calculate_weight_gradient!(rbm::RBM, h_pos::Mat{Float64}, v_pos::Mat{Float64}, h_neg::Mat{Float64}, v_neg::Mat{Float64}, lr::Float64; approx="CD")
-    # dW = zeros(size(rbm.W))
-    # dW = lr * ( (h_pos * v_pos') - (h_neg * v_neg') )
-    gemm!('N', 'T', lr, h_neg, v_neg, 0.0, rbm.dW)
-    gemm!('N', 'T', lr, h_pos, v_pos, -1.0, rbm.dW)
-
+    ## Load step buffer with negative-phase    
+    gemm!('N', 'T', lr, h_neg, v_neg, 0.0, rbm.dW)          # dW <- LearRate*<h_neg,v_neg>
+    ## Stubtract step buffer from positive-phase to get gradient    
+    gemm!('N', 'T', lr, h_pos, v_pos, -1.0, rbm.dW)         # dW <- LearnRate*<h_pos,v_pos> - dW
+    ## Second-Order EMF Correction (for EMF-TAP2, EMF-TAP3)
     if contains(approx,"tap") 
         buf2 = gemm('N', 'T', h_neg-abs2(h_neg), v_neg-abs2(v_neg)) .* rbm.W  
         axpy!(-lr, buf2, rbm.dW)
     end
-
+    ## Third-Order EMF Correction (for EMF-TAP3)
     if approx == "tap3"
         buf3 = gemm('N','T', (h_neg-abs2(h_neg)) .* (0.5-h_neg), (v_neg-abs2(v_neg)) .* (0.5-v_neg)) .* rbm.W2
         axpy!(-2.0*lr, buf3, rbm.dW)  
     end    
-
-    # rbm.dW += rbm.momentum * rbm.dW_prev
-    axpy!(rbm.momentum, rbm.dW_prev, rbm.dW)
-
-    # save current dW
-    # copy!(rbm.dW_prev, rbm.dW)
-    ##------------------------------------------##
-    # This content should be moved to the `fit_batch` function.
-    # axpy!(1.0, rbm.dW, rbm.W)           # rbm.W +=  dW
-    # rbm.W2=rbm.W.*rbm.W    
-    # if approx == "tap3"
-    #     rbm.W3=rbm.W2.*rbm.W
-    # end
-    ##------------------------------------------##
+    ## Apply Momentum (adding last gradient to this one)    
+    axpy!(rbm.momentum, rbm.dW_prev, rbm.dW)    # rbm.dW += rbm.momentum * rbm.dW_prev
 end
 
 function update_weights!(rbm::RBM,approx::AbstractString)
@@ -54,117 +42,20 @@ function update_weights!(rbm::RBM,approx::AbstractString)
  end
 
 function regularize_weight_gradient!(rbm::RBM,LearnRate::Float64;L2Penalty::Float64=NaN,L1Penalty::Float64=NaN,DropOutRate::Float64=NaN)
+    ## Quadratic penalty on weights (Energy shrinkage)
     if !isnan(L2Penalty)
         axpy!(-LearnRate*L2Penalty,rbm.W,rbm.dW)
     end
-
+    ## Linear penalty on weights (Sparsifying)
     if !isnan(L1Penalty)
         axpy!(-LearnRate*L1Penalty,sign(rbm.W),rbm.dW)
     end
-
+    ## Dropout Regularization (restricted set of updates)
     if !isnan(DropOutRate)
         # Not yet implemented, so we do nothing.
         # TODO: Implement Drop-out, here.
     end
 end
-
-##-----------------------------------------------------------------------------------------------------------##
-# This content should be depricated by shifting the regularization into the `fit_batch` function
-# function update_weights_QuadraticPenalty!(rbm, h_pos, v_pos, h_neg, v_neg, lr, decay_mag; approx="CD")
-#     # dW = zeros(size(rbm.W))
-    
-#     gemm!('N', 'T', lr, h_neg, v_neg, 0.0, rbm.dW)
-#     gemm!('N', 'T', lr, h_pos, v_pos, -1.0, rbm.dW)
-
-#     if contains(approx,"tap") 
-#         buf2 = gemm('N', 'T', h_neg-abs2(h_neg), v_neg-abs2(v_neg)) .* rbm.W  
-#         axpy!(-lr, buf2, rbm.dW)
-#     end
-
-#     if approx == "tap3"
-#         buf3 = gemm('N','T', (h_neg-abs2(h_neg)) .* (0.5-h_neg), (v_neg-abs2(v_neg)) .* (0.5-v_neg)) .* rbm.W2
-#         axpy!(-2.0*lr, buf3, rbm.dW)  
-#     end  
-
-#     # rbm.W += rbm.momentum * rbm.dW_prev
-#     axpy!(rbm.momentum, rbm.dW_prev, rbm.dW)
-
-#     # Apply Weight-Decay Penalty
-#     # rbm.W += -lr * L2-Penalty-Gradient
-#     axpy!(-lr*decay_mag,rbm.W,rbm.dW)
-
-#     # rbm.W +=  dW
-#     axpy!(1.0, rbm.dW, rbm.W)
-   
-#     rbm.W2=rbm.W.*rbm.W
-    
-#     if approx == "tap3"
-#         rbm.W3=rbm.W2.*rbm.W
-#     end
-#     # save current dW
-#     copy!(rbm.dW_prev, rbm.dW)
-# end
-
-# function update_weights_LinearPenalty!(rbm, h_pos, v_pos, h_neg, v_neg, lr, decay_mag ; approx="CD")
-#     # dW = zeros(size(rbm.W))
-#     # dW = (h_pos * v_pos') - (h_neg * v_neg')
-#     gemm!('N', 'T', lr, h_neg, v_neg, 0.0, rbm.dW)          # Not flushing rbm.dW since we multiply w/ 0.0
-#     gemm!('N', 'T', lr, h_pos, v_pos, -1.0,rbm.dW)
-
-#     if contains(approx,"tap") 
-#         buf2 = gemm('N', 'T', h_neg-abs2(h_neg), v_neg-abs2(v_neg)) .* rbm.W  
-#         axpy!(-lr, buf2, rbm.dW)
-#     end
-
-#     if approx == "tap3"
-#         buf3 = gemm('N','T', (h_neg-abs2(h_neg)) .* (0.5-h_neg), (v_neg-abs2(v_neg)) .* (0.5-v_neg)) .* rbm.W2
-#         axpy!(-2.0*lr, buf3, rbm.dW)  
-#     end  
-
-#     # rbm.W += rbm.momentum * rbm.dW_prev
-#     axpy!(rbm.momentum, rbm.dW_prev, rbm.dW)
-
-#     # Apply Weight-Decay Penalty
-#     # rbm.W += -lr * L1-Penalty-Gradient
-#     axpy!(-lr*decay_mag,sign(rbm.W),rbm.dW)
-
-#     # rbm.W += lr * dW
-#     axpy!(1.0, rbm.dW, rbm.W)
-    
-#     rbm.W2=rbm.W.*rbm.W
-    
-#     if approx == "tap3"
-#         rbm.W3=rbm.W2.*rbm.W
-#     end
-#     # save current dW
-#     copy!(rbm.dW_prev, rbm.dW)
-# end
-##-----------------------------------------------------------------------------------------------------------##
-
-# function contdiv(rbm::RBM, vis::Mat{Float64}, n_gibbs::Int; approx="CD")
-#     if approx == "CD"     
-#         v_pos, h_pos, v_neg, h_neg = gibbs(rbm, vis; n_times=n_gibbs)
-#     else
-#         v_pos, h_pos, v_neg, h_neg = iter_mag(rbm, vis; n_times=n_gibbs, approx=approx)
-#     end    
-#     return v_pos, h_pos, v_neg, h_neg
-# end
-
-
-# function persistent_contdiv(rbm::RBM, vis::Mat{Float64}, n_gibbs::Int; approx="CD")
-#     if approx == "CD"
-#         # take positive samples from real data
-#         v_pos, h_pos, _, _ = gibbs(rbm, vis; n_times=1)
-#         # take negative samples from "fantasy particles"
-#         _, _, v_neg, h_neg = gibbs(rbm, rbm.persistent_chain_vis; n_times=n_gibbs)
-#         copy!(rbm.persistent_chain_vis,v_neg)
-#         copy!(rbm.persistent_chain_hid,h_neg)
-#     else
-#         v_pos, h_pos, v_neg, h_neg = iter_mag_persist!(rbm, vis; n_times=n_gibbs, approx=approx)
-#     end    
-
-#     return v_pos, h_pos, v_neg, h_neg
-# end
 
 function get_negative_samples(rbm::RBM,vis_init::Mat{Float64},hid_init::Mat{Float64},approx::AbstractString, iterations::Int)
     if approx=="naive" || contains(approx,"tap")
@@ -272,6 +163,9 @@ end
                       [default=5]
 
     ## Returns
+     - *::RBM* -- A trained RBM model.
+     - *::Monitor* -- A Monitor structure containing information on the training progress over
+                      epochs.
 """
 function fit(rbm::RBM, X::Mat{Float64};
              persistent=true, lr=0.1, n_iter=10, batch_size=100, n_gibbs=1,
@@ -279,6 +173,7 @@ function fit(rbm::RBM, X::Mat{Float64};
              monitor_every=5,monitor_vis=false, approx="CD",
              persistent_start=1)
 
+    # TODO: This line needs to be changed to accomodate real-valued units
     @assert minimum(X) >= 0 && maximum(X) <= 1
 
     n_valid=0
@@ -322,13 +217,8 @@ function fit(rbm::RBM, X::Mat{Float64};
     # Scale the learning rate by the batch size
     lr=lr/batch_size
 
-    # Random initialization of the persistent chain
-    # It is okay if it isn't used in the actual training procedure.
-    p = shuffle!(collect(1:n_samples))[1:batch_size]
-    rbm.persistent_chain_vis = Array(Float64,n_features,batch_size)
-    for i=1:batch_size
-        rbm.persistent_chain_vis[:,i] = X[:,p[i]]
-    end
+    # Random initialization of the persistent chains
+    rbm.persistent_chain_vis,_ = random_columns(X,batch_size)
     rbm.persistent_chain_hid = ProbHidCondOnVis(rbm, rbm.persistent_chain_vis)
 
     use_persistent = false
@@ -355,7 +245,7 @@ function fit(rbm::RBM, X::Mat{Float64};
         walltime_µs=(toq()/n_batches/N)*1e6
         
         UpdateMonitor!(rbm,ProgressMonitor,X,itr;bt=walltime_µs,validation=validation)
-        ShowMonitor(rbm,ProgressMonitor,itr)
+        ShowMonitor(rbm,ProgressMonitor,X,itr)
     end
 
     return rbm, ProgressMonitor
